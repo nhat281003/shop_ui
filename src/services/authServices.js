a89ee6bea79ee7a55c1ea7880914450840f739d8
tomduyls <duybuiduc1@gmail.com>
2025-01-09 15:03:03 +0700

    refactor authService to use async/await for login method

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,10 +1,10 @@
-import baseApiService from "../base_api/api_service"
+import baseApiService from "../base_api/api_service";
 
-export const authService  = () => {
-    const useApi = baseApiService;
-    return {
-        login(data) {
-            useApi.post('/user/login', data);
-        },
-    }
-}
+export const authService = () => {
+  const useApi = baseApiService;
+  return {
+    login: async (data) => {
+      return await useApi.post("/user/login", data);
+    },
+  };
+};
